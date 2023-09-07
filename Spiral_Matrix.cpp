2661f24a0182bf768d6cc7497306fa8a6fae3f6d
Sleepy_Sage <kumarharsh073@gmail.com>
2023-09-07 15:15:02 +0530

    Most unordered array

--- Spiral_Matrix.cpp
+++ Spiral_Matrix.cpp
@@ -1,13 +1,46 @@
 #include <iostream>
 #include <vector>
 
+//My:
+//TC: O(m*n)
+//SC: O(1)
 class Solution {
 public:
-    std::vector<int> spiralOrder(std::vector<std::vector<int>>& matrix) 
-    {
-       
+    std::vector<int> spiralOrder(std::vector<std::vector<int>>& matrix) {
+        std::vector<int> ans;
+        int m = matrix.size();    
+        int n = matrix[0].size(); 
+        int left = 0, right = n - 1, top = 0, bottom = m - 1;
+
+        while (left <= right && top <= bottom) {
+            for (int j = left; j <= right; j++) {
+                ans.push_back(matrix[top][j]);
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int j = right; j >= left; j--) {
+                    ans.push_back(matrix[bottom][j]);
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
     }
 };
+
 
 int main()
 {
