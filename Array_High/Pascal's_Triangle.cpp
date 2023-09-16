0c100f7b5aa381262d51673c379d29f9cb92047f
Sleepy_Sage <kumarharsh073@gmail.com>
2023-09-16 17:15:22 +0530

    streak

--- Array_High/Pascal's_Triangle.cpp
+++ Array_High/Pascal's_Triangle.cpp
@@ -4,14 +4,29 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        return {{1,2,3},{4,5,6}};
+        vector<vector<int>> vec;
+        for(int i=0;i<numRows;i++)
+        {
+            int val=1;
+            vector<int> nums;
+            for(int j=0;j<=i;j++)
+            {
+                nums.push_back(val);
+                if (j != 0 && j != i-1) {
+                    val = vec[i - 1][j - 1] + vec[i - 1][j];
+                }
+
+            }
+            vec.push_back(nums);
+        }
+        return vec;
     }
 };
 
 int main() {
     int numRows;
     cin>>numRows;
-    cout<<numRows;
+    //cout<<numRows;
     Solution sol;
     vector<vector<int>> ans = sol.generate(numRows);
     for (int i = 0; i < ans.size(); i++) {
