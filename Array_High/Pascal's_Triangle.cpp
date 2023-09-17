c6e267c654bf15c4308a74c3006de9767bb08dba
Sleepy_Sage <kumarharsh073@gmail.com>
2023-09-17 23:52:23 +0530

    Pascal's triangle

--- Array_High/Pascal's_Triangle.cpp
+++ Array_High/Pascal's_Triangle.cpp
@@ -1,21 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+// TC : O(n^2)
+// SC : O(n^2)
+class Solution
+{
 public:
-    vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> generate(int numRows)
+    {
         vector<vector<int>> vec;
-        for(int i=0;i<numRows;i++)
+        for (int i = 0; i < numRows; i++)
         {
-            int val=1;
+            int val = 1;
             vector<int> nums;
-            for(int j=0;j<=i;j++)
+            for (int j = 0; j <= i; j++)
             {
-                nums.push_back(val);
-                if (j != 0 && j != i-1) {
+                if (i == 0 || i == 1 || j == 0 || j == i)
+                {
+                    val = 1;
+                }
+                else
+                {
                     val = vec[i - 1][j - 1] + vec[i - 1][j];
                 }
-
+                nums.push_back(val);
             }
             vec.push_back(nums);
         }
@@ -23,14 +31,17 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     int numRows;
-    cin>>numRows;
-    //cout<<numRows;
+    cin >> numRows;
+    // cout<<numRows;
     Solution sol;
     vector<vector<int>> ans = sol.generate(numRows);
-    for (int i = 0; i < ans.size(); i++) {
-        for (int j = 0; j < ans[i].size(); j++) {
+    for (int i = 0; i < ans.size(); i++)
+    {
+        for (int j = 0; j < ans[i].size(); j++)
+        {
             cout << ans[i][j] << " ";
         }
         cout << endl;
