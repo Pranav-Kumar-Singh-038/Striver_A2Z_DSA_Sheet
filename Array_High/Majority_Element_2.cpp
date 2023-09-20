00c7260787c10320a8290d5967ab35a340aef805
Sleepy_Sage <kumarharsh073@gmail.com>
2023-09-20 21:26:31 +0530

    Majority Element Optimal(N/3)

--- Array_High/Majority_Element_2.cpp
+++ Array_High/Majority_Element_2.cpp
@@ -1,26 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//TC: O(n)
-//SC: O(n)
-
+//Optimal
+//TC O(n)
+//SC O(1)
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-    vector<int> ans;
-    unordered_map<int,int> freq;
-    for(int i=0;i<nums.size();i++)
+    vector<int> majorityElement(vector<int>& nums) 
     {
-        freq[nums[i]]++;
-        if(freq[nums[i]]>nums.size()/3)
-        {
-            ans.push_back(nums[i]);
+        int num1 = INT_MIN, num2 = INT_MIN;
+        int count1 = 0, count2 = 0;
+        for(auto element : nums){
+            if(num1 == element){
+                count1++;
+            }
+            else if(num2 == element){
+                count2++;
+            }
+            else if(count1 == 0){
+                num1 = element;
+                count1 = 1;
+            }
+            else if(count2 == 0){
+                num2 = element;
+                count2 = 1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
         }
-    }
-    unordered_set<int> ans_set(ans.begin(), ans.end());
-    return vector<int>(ans_set.begin(), ans_set.end());
+      int countfin2=0;
+      int countfin1=0;
+      for(auto it:nums)
+      {
+        if(it==num1)
+        {
+            countfin1++;
+        }
+        if(it==num2)
+        {
+            countfin2++;
+        }
+      }
+      vector<int> ans;
+      if(countfin1>(nums.size()/3))
+      {
+        ans.push_back(num1);
+      }
+      if(countfin2>(nums.size()/3))
+        {
+       ans.push_back(num2);
+        }
+      return ans;
     }
 };
+
+//TC: O(n)
+//SC: O(n)
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//     vector<int> ans;
+//     unordered_map<int,int> freq;
+//     for(int i=0;i<nums.size();i++)
+//     {
+//         freq[nums[i]]++;
+//         if(freq[nums[i]]>nums.size()/3)
+//         {
+//             ans.push_back(nums[i]);
+//         }
+//     }
+//     unordered_set<int> ans_set(ans.begin(), ans.end());
+//     return vector<int>(ans_set.begin(), ans_set.end());
+//     }
+// };
 
 int main()
 {
