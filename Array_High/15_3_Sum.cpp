212f8b58089eb55781f3644d73afd91dcbf9748e
Sleepy_Sage <kumarharsh073@gmail.com>
2023-10-07 20:45:17 +0530

    4 Sum done with brute force

--- Array_High/15_3_Sum.cpp
+++ Array_High/15_3_Sum.cpp
@@ -16,7 +16,7 @@ public:
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < n - 2; i++) {
-            // Skip duplicates of nums[i]
+
             if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
                 int target = -nums[i];
                 int l = i + 1, r = n - 1;
@@ -30,7 +30,6 @@ public:
                         r--;
                     } else {
                         res.push_back({nums[i], nums[l], nums[r]});
-                        // Skip duplicates of nums[l] and nums[r]
                         while (l < r && nums[l] == nums[l + 1]) l++;
                         while (l < r && nums[r] == nums[r - 1]) r--;
                         l++;
--- Array_High/4Sum.cpp
+++ Array_High/4Sum.cpp
@@ -1,34 +1,71 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
 
-class Solution {
+class Solution
+{
 public:
-    std::vector<std::vector<int>> fourSum(std::vector<int>& nums, int target) {
-        // return {{1,2,3},{4,5,6}};
+    std::vector<std::vector<int>> fourSum(std::vector<int> &nums, int target)
+    {
+        std::vector<std::vector<int>> vec;
+        std::vector<long long int> copy(nums.begin(), nums.end());
+        unsigned long long int sum = 0;
+        if (nums.size() < 4)
+        {
+            return vec;
+        }
+        std::set<std::vector<int>> s;
+        for (int i = 0; i < nums.size() - 3; i++)
+        {
+            for (int j = i + 1; j < nums.size() - 2; j++)
+            {
+                for (int k = j + 1; k < nums.size() - 1; k++)
+                {
+                    for (int l = k + 1; l < nums.size(); l++)
+                    {
+                        sum = copy[i] + copy[j] + copy[k] + copy[l];
+                        if (sum == target)
+                        {
+                            std::vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                            sort(temp.begin(), temp.end());
+                            s.insert(temp);
+                        }
+                    }
+                }
+            }
+        }
+        for (auto it : s)
+        {
+            std::vector<int> temp(it.begin(), it.end());
+            vec.push_back(temp);
+        }
+        return vec;
     }
 };
 
 int main()
 {
     int n;
-    std::cin>>n;
+    std::cin >> n;
     std::vector<int> vec;
-    while(n--)
+    while (n--)
     {
-     int x;
-     std::cin>>x;
-     vec.push_back(x);
+        int x;
+        std::cin >> x;
+        vec.push_back(x);
     }
     int target;
-    std::cin>>target;
+    std::cin >> target;
     Solution sol;
-    std::vector<std::vector<int>> ans=sol.fourSum(vec,target);
-    for(auto it:ans)
+    std::vector<std::vector<int>> ans = sol.fourSum(vec, target);
+    for (auto it : ans)
     {
-        for(int i=0;i<it.size();i++)
+        // sort(it.begin(),it.end());
+        for (int i = 0; i < it.size(); i++)
         {
-            std::cout<<it[i]<<" ";
+            std::cout << it[i] << " ";
         }
-        std::cout<<"\n";
+        std::cout << "\n";
     }
 }
