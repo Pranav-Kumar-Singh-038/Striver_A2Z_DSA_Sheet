fa6075647a660d6075f9925dfc8ba2d585268df3
Sleepy_Sage <kumarharsh073@gmail.com>
2023-10-10 23:00:26 +0530

    4 sum naive, a bit improved approach

--- Array_High/4Sum.cpp
+++ Array_High/4Sum.cpp
@@ -2,47 +2,99 @@
 #include <vector>
 #include <algorithm>
 #include <set>
+// Time Complexity : O(N^3), Here Three nested loop creates the time complexity. Where N is the size of the
+// array(nums).
+// runtime: 274ms
 
+// Space Complexity : O(N), Hash Table(set) space.
+// memory used: 49.4 mb
+
+// Solved using Array(Three Nested Loop) + Sorting + Hash Table(set).
 class Solution
 {
 public:
     std::vector<std::vector<int>> fourSum(std::vector<int> &nums, int target)
     {
-        std::vector<std::vector<int>> vec;
-        std::vector<long long int> copy(nums.begin(), nums.end());
-        unsigned long long int sum = 0;
-        if (nums.size() < 4)
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        std::set<std::vector<int>> set;
+        std::vector<std::vector<int>> output;
+        for (int i = 0; i < n - 3; i++)
         {
-            return vec;
-        }
-        std::set<std::vector<int>> s;
-        for (int i = 0; i < nums.size() - 3; i++)
-        {
-            for (int j = i + 1; j < nums.size() - 2; j++)
+            for (int j = i + 1; j < n - 2; j++)
             {
-                for (int k = j + 1; k < nums.size() - 1; k++)
+                long long newTarget = (long long)target - (long long)nums[i] - (long long)nums[j];
+                int low = j + 1, high = n - 1;
+                while (low < high)
                 {
-                    for (int l = k + 1; l < nums.size(); l++)
+                    if (nums[low] + nums[high] < newTarget)
                     {
-                        sum = copy[i] + copy[j] + copy[k] + copy[l];
-                        if (sum == target)
-                        {
-                            std::vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
-                            sort(temp.begin(), temp.end());
-                            s.insert(temp);
-                        }
+                        low++;
+                    }
+                    else if (nums[low] + nums[high] > newTarget)
+                    {
+                        high--;
+                    }
+                    else
+                    {
+                        set.insert({nums[i], nums[j], nums[low], nums[high]});
+                        low++;
+                        high--;
                     }
                 }
             }
         }
-        for (auto it : s)
+        for (auto it : set)
         {
-            std::vector<int> temp(it.begin(), it.end());
-            vec.push_back(temp);
+            output.push_back(it);
         }
-        return vec;
+        return output;
     }
 };
+
+// My approach
+//  Time Complexity : O(N^4), Here Four nested loop creates the time complexity. Where N is the size of the
+//  array(nums).
+//  Space Complexity : O(N), Hash Table(set) space.
+//  Note : this will give TLE.
+//  class Solution
+//  {
+//  public:
+//      std::vector<std::vector<int>> fourSum(std::vector<int> &nums, int target)
+//      {
+//             int n = nums.size();
+//             sort(nums.begin(), nums.end());
+//                     std::set<std::vector<int>> s;
+//          std::vector<std::vector<int>> vec;
+//          if (nums.size() < 4)
+//          {
+//              return vec;
+//          }
+
+//         for (int i = 0; i < n - 3; i++)
+//         {
+//             for (int j = i + 1; j < n - 2; j++)
+//             {
+//                 for (int k = j + 1; k < n - 1; k++)
+//                 {
+//                     for (int l = k + 1; l < n; l++)
+//                     {
+//                         if((long long)nums[i] + (long long)nums[j] + (long long)nums[k] +
+//                         (long long)nums[l] == target)
+//                         {
+//                             s.insert({nums[i], nums[j], nums[k], nums[l]});
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//         for (auto it : s)
+//         {
+//             vec.push_back(it);
+//         }
+//         return vec;
+//     }
+// };
 
 int main()
 {
