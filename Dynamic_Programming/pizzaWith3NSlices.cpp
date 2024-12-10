4333f76b436397482eb71a2a5014825e0b66ea2e
Pranav <kumarharsh073@gmail.com>
2024-12-10 14:32:59 +0530

    changes

--- Dynamic_Programming/pizzaWith3NSlices.cpp
+++ Dynamic_Programming/pizzaWith3NSlices.cpp
@@ -4,19 +4,19 @@ public:
         if (picksLeft == 0 || index > endIndex) {
             return 0;
         }
-        if(memo[index][picksLeft]!=-1)
+        if(memo[index][picksLeft-1]!=-1)
         {
-            return memo[index][picksLeft];
+            return memo[index][picksLeft-1];
         }
         int take = slices[index] + solve(slices, picksLeft - 1, index + 2, endIndex, memo);
         int skip = 0 + solve(slices, picksLeft, index + 1, endIndex, memo);
-        return memo[index][picksLeft]=max(take, skip);
+        return memo[index][picksLeft-1]=max(take, skip);
     }
 
     int maxSizeSlices(vector<int>& slices) {
         int picks = slices.size() / 3;
-        vector<vector<int>> memo1(slices.size(), vector<int>(picks+1, -1));
-        vector<vector<int>> memo2(slices.size(), vector<int>(picks+1, -1));
+        vector<vector<int>> memo1(slices.size(), vector<int>(picks, -1));
+        vector<vector<int>> memo2(slices.size(), vector<int>(picks, -1));
         int scen1 = solve(slices, picks, 0, slices.size() - 2, memo1);
         int scen2 = solve(slices, picks, 1, slices.size() - 1, memo2);
         return max(scen1, scen2);
